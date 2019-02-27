ea2e849b7c608f014109dc5cc77310cca54d86cf
beogip <beogip@gmail.com>
2019-02-27 19:25:52 -0300

    feat(ddmin): create delta debuging function

---
+++ ddmin.py
@@ -0,0 +1,24 @@
+def ddmin(func, s):
+    assert callable(func)
+    n = 2     # Initial granularity
+    while len(s) >= 2:
+        start = 0
+        subset_length = len(s) / n
+        some_complement_is_failing = False
+
+        while start < len(s):
+            complement = s[:start] + s[start + subset_length:]
+
+            if func(complement) == "FAIL":
+                s = complement
+                n = max(n - 1, 2)
+                some_complement_is_failing = True
+                break
+                
+            start += subset_length
+
+        if not some_complement_is_failing:
+            if n == len(s):
+                break
+            n = min(n * 2, len(s))
+    return s
